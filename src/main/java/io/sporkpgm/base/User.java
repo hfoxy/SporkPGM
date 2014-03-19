1d1b4cdca513d30c45855c3b133df9ccd723f596
RainoBoy97 <rainoboy97@gmail.com>
2014-03-19 12:29:18 +0100

    Some methods in User

--- src/main/java/io/sporkpgm/base/User.java
+++ src/main/java/io/sporkpgm/base/User.java
@@ -1,5 +1,25 @@
 package io.sporkpgm.base;
 
+import org.bukkit.entity.Player;
+
 public class User {
 
+	private Player player;
+	
+	public User(Player player) {
+		this.player = player;
+	}
+	
+	public Player getPlayer() {
+		return player;
+	}
+	
+	public String getName() {
+		return getPlayer().getName();
+	}
+	
+	public void sendMessage(String message) {
+		getPlayer().sendMessage(message);
+	}
+	
 }
