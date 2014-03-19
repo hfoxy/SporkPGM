ba592f4aca241d6a4330cc56ac165894b8d650b5
ParaPenguin <harry.fox.ic@icloud.com>
2014-03-19 18:14:00 +0000

    Default Config and some Maps repository stuff

--- src/main/java/io/sporkpgm/Spork.java
+++ src/main/java/io/sporkpgm/Spork.java
@@ -1,11 +1,14 @@
 package io.sporkpgm;
 
+import io.sporkpgm.map.MapBuilder;
+import io.sporkpgm.map.MapLoader;
 import io.sporkpgm.map.SporkMap;
 import io.sporkpgm.module.Module;
 import io.sporkpgm.module.ModuleBuilder;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.dom4j.Document;
 
+import java.io.File;
 import java.lang.reflect.Constructor;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,10 +18,31 @@ public class Spork extends JavaPlugin {
 	protected static Spork spork;
 
 	protected List<Class<? extends ModuleBuilder>> builders;
+	protected List<MapBuilder> maps;
+	protected File repository;
 
 	@Override
 	public void onEnable() {
 		spork = this;
+		saveDefaultConfig();
+
+		if(getConfig().getString("settings.maps.respository") == null) {
+			getConfig().set("settings.maps.respository", "maps/");
+		}
+
+		repository = new File(getConfig().getString("settings.maps.respository"));
+		maps = new ArrayList<>();
+
+		String startup = getConfig().getString("settings.maps.startup");
+		StartupType type = StartupType.getType(startup);
+
+		List<MapBuilder> load = MapLoader.build(repository);
+		if(type == StartupType.SPECIFIED) {
+			List<String> specified = getConfig().getStringList("settings.maps.load");
+			if(specified != null) {
+
+			}
+		}
 	}
 
 	public List<Module> getModules(Document document) {
@@ -53,6 +77,45 @@ public class Spork extends JavaPlugin {
 
 	public static Spork get() {
 		return spork;
+	}
+
+	public enum StartupType {
+
+		ALL(new String[]{"all"}),
+		SPECIFIED(new String[]{"specified", "specify", "listed"});
+
+		String[] names;
+
+		StartupType(String[] names) {
+			this.names = names;
+		}
+
+		public String[] getNames() {
+			return names;
+		}
+
+		public boolean matches(String name) {
+			for(String value : names) {
+				if(value.equalsIgnoreCase(name)) {
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		public static StartupType getType(String name) {
+			if(name != null) {
+				for(StartupType type : values()) {
+					if(type.matches(name)) {
+						return type;
+					}
+				}
+			}
+
+			return ALL;
+		}
+
 	}
 
 }
---
+++ src/main/java/io/sporkpgm/map/MapLoader.java
@@ -0,0 +1,13 @@
+package io.sporkpgm.map;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MapLoader {
+
+	public static List<MapBuilder> build(File folder) {
+		return new ArrayList<>();
+	}
+
+}
