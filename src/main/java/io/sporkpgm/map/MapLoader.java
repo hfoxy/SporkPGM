59557ca9d6591b81fb060acd12c5cd8b2ace1840
ParaPenguin <harry.fox.ic@icloud.com>
2014-03-21 20:07:54 +0000

    It would help if it actually tried to load the maps...

--- src/main/java/io/sporkpgm/map/MapLoader.java
+++ src/main/java/io/sporkpgm/map/MapLoader.java
@@ -1,5 +1,11 @@
 package io.sporkpgm.map;
 
+import io.sporkpgm.util.Config;
+import io.sporkpgm.util.Config.Map;
+import io.sporkpgm.util.Log;
+import org.dom4j.Document;
+import org.dom4j.io.SAXReader;
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
@@ -7,7 +13,35 @@ import java.util.List;
 public class MapLoader {
 
 	public static List<MapBuilder> build(File folder) {
-		return new ArrayList<>();
+		List<MapBuilder> builders = new ArrayList<>();
+
+		File[] maps = Map.DIRECTORY.listFiles();
+		Log.info("Found " + maps.length + " possible entries in the Maps respository");
+
+		for(File map : maps) {
+			if(!map.isDirectory()) {
+				Log.info(map.getName() + " is not a directory");
+				continue;
+			}
+
+			File xml = new File(map, "map.xml");
+			File region = new File(map, "region");
+			File level = new File(map, "level.dat");
+
+			boolean loadable = xml.exists() && (region.exists() && region.isDirectory()) && level.exists();
+			if(loadable) {
+				Log.info(map.getName() + " is loadable!");
+				try {
+					SAXReader reader = new SAXReader();
+					Document document = reader.read(xml);
+					builders.add(new MapBuilder(document, folder));
+				} catch(Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+		}
+
+		return builders;
 	}
 
 }
