ca2dcd53682fa6e4c210336a4568d28c3cc79bc3
ParaPenguin <harry.fox.ic@icloud.com>
2014-03-23 08:48:41 +0000

    Switch them back

--- src/main/java/io/sporkpgm/filter/FilterBuilder.java
+++ src/main/java/io/sporkpgm/filter/FilterBuilder.java
@@ -147,22 +147,22 @@ public class FilterBuilder {
 	private static List<Filter> defaults() {
 		List<Filter> defaults = new ArrayList<>();
 
-		AccessCondition allowPlayers = new AccessCondition("allow-players", State.ALLOW, null);
+		AccessCondition allowPlayers = new AccessCondition("allow-players", State.DENY, null);
 		defaults.add(allowPlayers);
 		MultiCondition denyPlayers = new MultiCondition("deny-players", State.DENY, Modifier.NOT, allowPlayers);
 		defaults.add(denyPlayers);
 
-		WorldCondition allowBlocks = new WorldCondition("allow-blocks", State.ALLOW);
+		WorldCondition allowBlocks = new WorldCondition("allow-blocks", State.DENY);
 		defaults.add(allowBlocks);
 		MultiCondition denyBlocks = new MultiCondition("deny-blocks", State.DENY, Modifier.NOT, allowBlocks);
 		defaults.add(denyBlocks);
 
-		BlockCondition allowWorld = new BlockCondition("allow-world", State.ALLOW, null);
+		BlockCondition allowWorld = new BlockCondition("allow-world", State.DENY, null);
 		defaults.add(allowWorld);
 		MultiCondition denyWorld = new MultiCondition("deny-world", State.DENY, Modifier.NOT, allowWorld);
 		defaults.add(denyWorld);
 
-		EntityCondition allowEntities = new EntityCondition("allow-entities", State.ALLOW, null);
+		EntityCondition allowEntities = new EntityCondition("allow-entities", State.DENY, null);
 		defaults.add(allowEntities);
 		MultiCondition denyEntities = new MultiCondition("deny-entities", State.DENY, Modifier.NOT, allowEntities);
 		defaults.add(denyEntities);
