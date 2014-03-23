f53b6452f619e18b54554755c5590a77939a3dab
ParaPenguin <harry.fox.ic@icloud.com>
2014-03-23 09:51:17 +0000

    Add deny-all & allow-all

--- src/main/java/io/sporkpgm/filter/FilterBuilder.java
+++ src/main/java/io/sporkpgm/filter/FilterBuilder.java
@@ -170,6 +170,13 @@ public class FilterBuilder {
 		defaults.add(allowEntities);
 		MultiCondition denyEntities = new MultiCondition("deny-entities", State.DENY, Modifier.NOT, allowEntities);
 		defaults.add(denyEntities);
+		
+		Condition[] allow = new Condition[]{ allowPlayers, allowBlocks, allowWorld, allowEntities };
+		MultiCondition allowAll = new MultiCondition("allow-all", State.DENY, Modifier.ANY, allow);
+		defaults.add(allowAll);
+		Condition[] deny = new Condition[]{ denyPlayers, denyBlocks, denyWorld, denyEntities };
+		MultiCondition denyAll = new MultiCondition("deny-all", State.DENY, Modifier.ANY, deny);
+		defaults.add(denyAll);
 
 		/*
 		EntityCondition allowMobs = new EntityCondition("allow-entities", State.DENY, EntityType);
