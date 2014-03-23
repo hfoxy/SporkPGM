5b430c17a32f8dcc3c62ddd785129f9ba32d5377
ParaPenguin <harry.fox.ic@icloud.com>
2014-03-23 22:59:44 +0000

    Fix later

--- src/main/java/io/sporkpgm/map/SporkMap.java
+++ src/main/java/io/sporkpgm/map/SporkMap.java
@@ -467,13 +467,27 @@ public class SporkMap {
 	}
 
 	public List<Region> getContainingRegions(Location location) {
+		BlockRegion block = new BlockRegion(location.getBlockX(), location.getBlockY(), location.getBlockZ());
+		if(getName().equalsIgnoreCase("Race for Victory 2")) {
+			String[] checks = new String[]{"blue-wool-rooms", "red-wool-rooms"};
+			for(String check : checks) {
+				Region region = getRegion(check);
+				if(region == null) {
+					Log.info(check + " does not exist!");
+					continue;
+				}
+				Log.info("Checking '" + region.getName() + "' (" + region.getClass().getSimpleName() + ") for " + block);
+
+				Region red_wool_rooms = getRegion("red-wool-rooms");
+			}
+		}
+
 		List<Region> regionList = new ArrayList<>();
 		for(Region region : regions) {
-			BlockRegion block = new BlockRegion(location.getBlockX(), location.getBlockY(), location.getBlockZ());
 			// Log.info("Checking '" + region.getName() + "' (" + region.getClass().getSimpleName() + ") for " + block);
 
 			if(region.isInside(block)) {
-				Log.info(block + " was found inside '" + region.getName() + "' (" + region.getClass().getSimpleName() + ")");
+				// Log.info(block + " was found inside '" + region.getName() + "' (" + region.getClass().getSimpleName() + ")");
 				regionList.add(region);
 			}
 		}
