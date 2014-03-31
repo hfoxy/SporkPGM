bec29ce45eb4118964ee2938acfbf8b6c9824d83
ParaPenguin <harry.fox.ic@icloud.com>
2014-03-31 18:46:14 +0100

    Allow for different ways of parsing spawns from the XML

--- src/main/java/io/sporkpgm/team/spawns/SporkSpawnBuilder.java
+++ src/main/java/io/sporkpgm/team/spawns/SporkSpawnBuilder.java
@@ -24,59 +24,70 @@ public class SporkSpawnBuilder {
 		for(Element spawns : XMLUtil.getElements(root, "spawns")) {
 			String teamS = spawns.attributeValue("team");
 
-			int noname = 1;
-			for(Element element : XMLUtil.getElements(spawns, "spawn", "default")) {
-				String nameS = null;
-
-				teamS = (element.attributeValue("team") != null ? element.attributeValue("team") : teamS);
-				if(element.getName().equalsIgnoreCase("default")) {
-					teamS = map.getObservers().getName();
-				}
-
-				String yawS = element.attributeValue("yaw");
-				String pitchS = element.attributeValue("pitch");
-				String kitS = element.attributeValue("kit");
-
-				String name = (nameS == null ? "noname-" + noname : nameS);
-				SporkTeam team = map.getTeam(teamS);
-
-				List<Region> regions = RegionBuilder.parseSubRegions(element);
-				float yaw = 0;
-				float pitch = 0;
-
-				try {
-					yaw = Float.parseFloat(yawS);
-				} catch(Exception ignored) {
-				}
-
-				try {
-					pitch = Float.parseFloat(pitchS);
-				} catch(Exception ignored) {
-				}
-
-				SporkKit match = null;
-				if(kitS != null) {
-					for(SporkKit ak : map.getKits()) {
-						if(ak.getName().equalsIgnoreCase(kitS)) {
-							match = ak;
-							break;
-						}
-					}
-					if(match == null)
-						throw new ModuleLoadException("Kit `" + kitS + "` not found while parsing `" + teamS + "` spawn!");
-				}
-
-				if(nameS == null) {
-					noname++;
-				}
-
-				SporkSpawn spawn = new SporkSpawn(name, regions, match, yaw, pitch);
-				team.getSpawns().add(spawn);
-				sporks.add(spawn);
+			sporks.addAll(parseSpawns(map, XMLUtil.getElements(spawns, "spawn", "default"), teamS));
+			if(spawns.element("spawns") != null) {
+				sporks.addAll(parseSpawns(map, XMLUtil.getElements(spawns.element("spawns"), "spawn", "default"), teamS));
 			}
 		}
 
 		return sporks;
+	}
+
+	public static List<SporkSpawn> parseSpawns(SporkMap map, List<Element> spawns, String teamS) throws ModuleLoadException, InvalidRegionException {
+		List<SporkSpawn> sporks = new ArrayList<>();
+
+		for(Element element : spawns) {
+			SporkSpawn spawn = parseSpawn(map, element, teamS);
+			sporks.add(spawn);
+		}
+
+		return sporks;
+	}
+
+	public static SporkSpawn parseSpawn(SporkMap map, Element element, String teamS) throws ModuleLoadException, InvalidRegionException {
+		String nameS = null;
+
+		teamS = (element.attributeValue("team") != null ? element.attributeValue("team") : teamS);
+		if(element.getName().equalsIgnoreCase("default")) {
+			teamS = map.getObservers().getName();
+		}
+
+		String yawS = element.attributeValue("yaw");
+		String pitchS = element.attributeValue("pitch");
+		String kitS = element.attributeValue("kit");
+
+		String name = (nameS == null ? "noname" : nameS);
+		SporkTeam team = map.getTeam(teamS);
+
+		List<Region> regions = RegionBuilder.parseSubRegions(element);
+		float yaw = 0;
+		float pitch = 0;
+
+		try {
+			yaw = Float.parseFloat(yawS);
+		} catch(Exception ignored) {
+		}
+
+		try {
+			pitch = Float.parseFloat(pitchS);
+		} catch(Exception ignored) {
+		}
+
+		SporkKit match = null;
+		if(kitS != null) {
+			for(SporkKit ak : map.getKits()) {
+				if(ak.getName().equalsIgnoreCase(kitS)) {
+					match = ak;
+					break;
+				}
+			}
+			if(match == null)
+				throw new ModuleLoadException("Kit `" + kitS + "` not found while parsing `" + teamS + "` spawn!");
+		}
+
+		SporkSpawn spawn = new SporkSpawn(name, regions, match, yaw, pitch);
+		team.getSpawns().add(spawn);
+		return spawn;
 	}
 
 }
