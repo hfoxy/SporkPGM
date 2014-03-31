692bb544dc269b16fbd34a7277e47c48bd87480f
ParaPenguin <harry.fox.ic@icloud.com>
2014-03-31 20:52:53 +0100

    Prevent friendly fire from teammates arrows

--- src/main/java/io/sporkpgm/module/modules/friendlyfire/FriendlyFireModule.java
+++ src/main/java/io/sporkpgm/module/modules/friendlyfire/FriendlyFireModule.java
@@ -6,6 +6,7 @@ import io.sporkpgm.module.builder.Builder;
 import io.sporkpgm.player.SporkPlayer;
 import io.sporkpgm.team.SporkTeam;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
@@ -20,13 +21,35 @@ public class FriendlyFireModule extends Module {
 	}
 
 	@EventHandler(priority = EventPriority.HIGH)
-	public void onPlayerDamageEvent(EntityDamageByEntityEvent e) {
-		if(e.getDamager() instanceof Player && e.getEntity() instanceof Player && !enabled) {
-			SporkTeam attackerTeam = SporkPlayer.getPlayer((Player) e.getDamager()).getTeam();
-			SporkTeam victimTeam = SporkPlayer.getPlayer((Player) e.getEntity()).getTeam();
-			if(victimTeam == attackerTeam) {
-				e.setCancelled(true);
+	public void onPlayerDamageEvent(EntityDamageByEntityEvent event) {
+		if(enabled) {
+			return;
+		}
+
+		Player entity = null;
+		Player damager = null;
+
+		if(event.getEntity() instanceof Player) {
+			entity = (Player) event.getEntity();
+		}
+
+		if(event.getDamager() instanceof Player) {
+			damager = (Player) event.getDamager();
+		} else if(event.getDamager() instanceof Projectile) {
+			Projectile projectile = (Projectile) event.getDamager();
+			if(projectile.getShooter() instanceof Player) {
+				damager = (Player) projectile.getShooter();
 			}
+		}
+
+		if(damager == null || entity == null) {
+			return;
+		}
+
+		SporkTeam victimTeam = SporkPlayer.getPlayer(entity).getTeam();
+		SporkTeam attackerTeam = SporkPlayer.getPlayer(damager).getTeam();
+		if(victimTeam == attackerTeam) {
+			event.setCancelled(true);
 		}
 	}
 
